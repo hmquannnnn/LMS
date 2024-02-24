10d3a7e924db23b928b89e9da06006e8eca36b3d
monkeydminh49 <nguyendangminh49@gmail.com>
2024-02-25 06:40:50 +0700

    Update verify-email

--- src/app/verify-email/page.tsx
+++ src/app/verify-email/page.tsx
@@ -1,23 +1,47 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { callVerifyEmail } from '@/apis/authAPI';
-import { useSearchParams } from 'next/navigation';
-
+import { useSearchParams, useRouter } from 'next/navigation';
+import { Spin } from 'antd';
 
 const VerifyEmailPage = () => {
     const searchParams = useSearchParams();
     const code: String | null = searchParams.get('code');
+    const [isVerify, setIsVerify] = useState(false);
+    const router = useRouter();
 
     useEffect(() => {
         callVerifyEmail(code).then((res) => {
             console.log(res)
-        });
+        }).then((res) => {
+            if (res.status == 'ok') {
+                router.push('/login')
+                setIsVerify(true)
+            }
+        }).catch;
     }, []);
 
     return (
-        <div>Verifying email...</div>
+        <div className='w-full h-[100vh] flex flex-col gap-5 items-center justify-center'>
+            {
+                !isVerify &&
+                <>
+                    <div>
+                        <Spin />
+                    </div>
+                    <div className='w-fit text-2xl'>Đang xác thực email của bạn...</div>
+                </>
+            }
+            {
+                isVerify &&
+                <>
+                    <div className='w-fit text-2xl'>Đã xãc thực thành công email của bạn.</div>
+                </>
+            }
 
+
+        </div>
     )
 
 }
