b728f8f4da5816ffb0c5f4b53a079c5f9add7e65
hmquannnnn <minhquanhoang040103@gmail.com>
2024-02-25 07:45:27 +0700

    fix: verify email

--- src/app/verify-email/page.tsx
+++ src/app/verify-email/page.tsx
@@ -1,48 +1,45 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
-import { callVerifyEmail } from '@/apis/authAPI';
-import { useSearchParams, useRouter } from 'next/navigation';
-import { Spin } from 'antd';
+import { useEffect, useState } from "react";
+import { callVerifyEmail } from "@/apis/authAPI";
+import { useRouter, useSearchParams } from "next/navigation";
+import { Spin } from "antd";
 
 const VerifyEmailPage = () => {
-    const searchParams = useSearchParams();
-    const code: String | null = searchParams.get('code');
-    const [isVerify, setIsVerify] = useState(false);
-    const router = useRouter();
+  const searchParams = useSearchParams();
+  const code: String | null = searchParams.get("code");
+  const [isVerify, setIsVerify] = useState(false);
+  const router = useRouter();
+  const verifyEmail = async () => {
+    const res = await callVerifyEmail(code);
+    console.log(res);
+    if (res.status === "ok") {
+      router.push("/login");
+      setIsVerify(true);
+    }
+  };
+  useEffect(() => {
+    verifyEmail();
+  }, []);
 
-    useEffect(() => {
-        callVerifyEmail(code).then((res) => {
-            console.log(res)
-        }).then((res) => {
-            if (res.status == 'ok') {
-                router.push('/login')
-                setIsVerify(true)
-            }
-        }).catch;
-    }, []);
-
-    return (
-        <div className='w-full h-[100vh] flex flex-col gap-5 items-center justify-center'>
-            {
-                !isVerify &&
-                <>
-                    <div>
-                        <Spin />
-                    </div>
-                    <div className='w-fit text-2xl'>Đang xác thực email của bạn...</div>
-                </>
-            }
-            {
-                isVerify &&
-                <>
-                    <div className='w-fit text-2xl'>Đã xãc thực thành công email của bạn.</div>
-                </>
-            }
-
-
-        </div>
-    )
-
-}
+  return (
+    <div className="w-full h-[100vh] flex flex-col gap-5 items-center justify-center">
+      {!isVerify && (
+        <>
+          <div>
+            <Spin />
+          </div>
+          <div className="w-fit text-2xl">Đang xác thực email của bạn...</div>
+        </>
+      )}
+      {isVerify && (
+        <>
+          <div className="w-fit text-2xl">
+            Đã xãc thực thành công email của bạn.
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 export default VerifyEmailPage;
